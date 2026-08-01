ac8225d47bfc7278462ce721ff01c5e03d5246f6
0cirilolucas23 <lucas.cirilogomes@gmail.com>
2026-08-01 20:42:48 -0300

    Fix: Planilha e dados do make

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Growfy LaunchOS",
-  description: "Dashboard inteligente para gestão de lançamentos digitais",
+  description: "Dashboard inteligente para gestão de produtos digitais",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
